3c3e629c5f9b8f05a7f5bfb361845c3731d6fb2d
Anu V S <anuvsooralivila@gmail.com>
2023-05-31 14:14:52 +0530

    Movie section added

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 4000,
       disableOnInteraction: false,
     },
     pagination: {

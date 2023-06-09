4462f6c2f6ce63b71f7d4cf13932a646aa383558
Anu V S <anuvsooralivila@gmail.com>
2023-06-09 15:22:30 +0530

    Compleate all sections, not footer

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ var swiper = new Swiper(".home", {
     },
     
   });
+
+
+  /*swiper code for coming soon */
+  var swiper = new Swiper(".coming-container", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 55000,
+      disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints:{
+      0: {
+        slidesPerView: 2,
+      },
+      568: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      968: {
+        slidesPerView: 5,
+      },
+    },
+  });
+

82460ba4557a8b0e932e6ac540c34d7a6b09c3c5
Anu V S <anuvsooralivila@gmail.com>
2023-06-13 21:40:28 +0530

    set navbar menu

--- main.js
+++ main.js
@@ -2,11 +2,23 @@
 
 // header off code
 let header= document.querySelector('header');
+let menu= document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+
 window.addEventListener('scroll', ()=>{
   header.classList.toggle('shadow',window.scrollY>0);
 });
 
+menu.onclick = () =>{
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
 
+// to remove toggle data while user scroll
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
 
 
 /*swiper code */
